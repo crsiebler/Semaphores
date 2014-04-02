de909d6649b8bff5ead8be2d8bfa78a925f39c3c
Cory R Siebler <cory.siebler@gmail.com>
2014-04-01 18:56:10 -0700

    Added Notes to Main File (i.e. proj-3.c)

---
+++ proj-3.c
@@ -0,0 +1,19 @@
+// Name: Cory Siebler & Marcus Finney
+// Class: CSE430 #12109
+// Assignment: Project 3
+// Description: Main file which holds the threads for Producer-Consumer.
+
+//---------------//
+// Include Files //
+//---------------//
+
+//--------------------//
+// Threaded Functions //
+//--------------------//
+
+//-------------//
+// main Method //
+//-------------//
+int main() {
+	return 0;
+}
